92188e5c5d9aaa04593e1d7fdc3c81a306e6dac8
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-06-21 18:11:53 +0200

    Add @unform to SignIn/Up forms

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,30 +9,38 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="Company Logo" />
-      <form>
-        <h1>Create an Account</h1>
-        <Input name="name" type="text" icon={FiUser} placeholder="Name" />
-        <Input name="email" type="text" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          type="password"
-          icon={FiLock}
-          placeholder="Password"
-        />
-        <Button type="submit">Continue</Button>
-      </form>
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="Company Logo" />
+        <Form
+          // initialData={{ name: 'Your name here' }}
+          onSubmit={handleSubmit}
+        >
+          <h1>Create an Account</h1>
+          <Input name="name" type="text" icon={FiUser} placeholder="Name" />
+          <Input name="email" type="text" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            type="password"
+            icon={FiLock}
+            placeholder="Password"
+          />
+          <Button type="submit">Continue</Button>
+        </Form>
 
-      <a href="new">
-        <FiArrowLeft />
-        Already have an account?
-      </a>
-    </Content>
-  </Container>
-);
+        <a href="new">
+          <FiArrowLeft />
+          Already have an account?
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
